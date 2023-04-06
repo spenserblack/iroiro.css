8fb21defa8aa8df77d6a3ff4ddfbc44aa2567237
Spenser Black <spenserblack01@gmail.com>
2023-04-06 15:26:40 +0000

    Document programmatic coloring

--- docs/src/pages/Colors.vue
+++ docs/src/pages/Colors.vue
@@ -15,16 +15,34 @@ const colorNames = colors.map(({ romanized }) => romanized.toLowerCase());
     <h3><code>.text-*</code></h3>
     <div class="preview-container">
       <div v-for="name in colorNames" :key="name" class="row">
-        <div class="on-white col" :class="`text-${name}`">.text-{{ name }}</div>
-        <div class="on-black col" :class="`text-${name}`">.text-{{ name }}</div>
+        <div class="on-white col preview-col" :class="`text-${name}`">.text-{{ name }}</div>
+        <div class="on-black col preview-col" :class="`text-${name}`">.text-{{ name }}</div>
       </div>
     </div>
 
     <h3><code>.bg-*</code></h3>
     <div class="preview-container">
       <div v-for="name in colorNames" :key="name" class="row">
-        <div class="white col" :class="`bg-${name}`">.bg-{{ name }}</div>
-        <div class="black col" :class="`bg-${name}`">.bg-{{ name }}</div>
+        <div class="white col preview-col" :class="`bg-${name}`">.bg-{{ name }}</div>
+        <div class="black col preview-col" :class="`bg-${name}`">.bg-{{ name }}</div>
+      </div>
+    </div>
+  </div>
+
+  <h3>Programmatically selecting colors</h3>
+  <p>
+    <a :href="$iroiro">iroiro</a> provides <code>colors: Color[]</code> and
+    <code>getColors(name: string): Color[]</code>, which can aid in selecting colors.
+  </p>
+  <h4>Example</h4>
+  <div class="preview-container">
+    <div class="row">
+      <div class="col">
+        <pre>
+          <code>const red = iroiro.getColors('red');</code>
+          <code>// NOTE: These stylesheets use the romanized name</code>
+          <code>const cssClass = `text-${red[0].romanized}`;</code>
+        </pre>
       </div>
     </div>
   </div>
@@ -49,8 +67,11 @@ const colorNames = colors.map(({ romanized }) => romanized.toLowerCase());
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 50%;
-    padding: $vertical-padding 0;
+
+    &.preview-col {
+      width: 50%;
+      padding: $vertical-padding 0;
+    }
 
     &.on-black {
       background-color: #000;
